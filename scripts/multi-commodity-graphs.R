bc110173134e784ccdbe68ae2d975934c52f43d2
siwar149 <siwar@workmail.com>
2025-04-10 14:31:07 +0000

    advancements on graphs for my paper

--- scripts/multi-commodity-graphs.R
+++ scripts/multi-commodity-graphs.R
@@ -9,13 +9,17 @@ library(pandoc)
 library(rmarkdown)
 library(RColorBrewer)
 library(ggsankeyfier)
+library(sf)
+library(units)
+library(ggpubr)
+library(rnaturalearthdata)
 
 # Install ggsankey from GitHub
-devtools::install_github("davidsjoberg/ggsankey")
+#devtools::install_github("davidsjoberg/ggsankey")
 library(ggsankey)
 
 # set working directory
-setwd("~/projects/FAO-deforestation/")
+setwd("~/projects/world-deforestation//")
 setwd("~/deforestation_accounting/")
 
 results_all <- readRDS("./output/global_results_v3.rds")
@@ -34,19 +38,102 @@ pop_d <- WDI(indicator = "SP.POP.TOTL", country = "all", start = 2010, end = 202
 # preliminary analysis
 
 # setting the data
+results_all <- results_all[, `:=` (continent_origin = fifelse(continent_origin == 'EU', 'EUR',continent_origin),
+                                   continent_imd = fifelse(continent_imd == 'EU', 'EUR',continent_imd),
+                                   continent_target = fifelse(continent_target == 'EU', 'EUR',continent_target))]
+
+# general graph
+results_all %>% 
+  group_by(consumption_category,trade) %>% summarise(d_f_a = sum(d_f_a), .groups = "drop") %>%
+  mutate(prop = d_f_a / sum(d_f_a) * 100) %>%
+  arrange(desc(interaction(trade, consumption_category))) %>%
+  mutate(ypos = cumsum(prop) - 0.5 * prop) %>%
+  ggplot(aes(x = "", y = d_f_a, fill = interaction(consumption_category,trade))) +
+  geom_bar(stat = "identity", width = 1) +  # Bar chart
+  coord_polar(theta = "y") +
+  geom_text(aes(y = ypos, label = paste0(round(prop), "%")), 
+            color = "white", size = 5) +# Convert to pie
+  theme_void() +
+  theme(legend.title = element_blank())
+
+# I like this graph (INTEGRATE WITH WORLD MAP!)
+results_all %>%
+  filter(continent_target != "ROW") %>%
+  mutate(trade = if_else(continent_origin == continent_target, "DOM", "TRADE")) %>%
+  group_by(continent_target,consumption_category, trade) %>% 
+  summarise(d_f_a = sum(d_f_a), .groups = "drop") %>%
+  mutate(prop = d_f_a / sum(d_f_a) * 100) %>%
+  arrange(interaction(trade, consumption_category)) %>%
+  mutate(ypos = cumsum(d_f_a) - 0.5 * d_f_a) %>%
+  ggplot(aes(x = "", y = d_f_a, fill = interaction(consumption_category, trade))) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar(theta = "y") +
+  #geom_text(aes(y = ypos, label = paste0(round(prop), "%")), 
+  #          color = "white", size = 5) +
+  scale_fill_viridis_d(option = "H", direction = 1, begin = 0.4) +
+  facet_wrap(~continent_target, scales = "free") +
+  theme_void() +
+  theme(legend.title = element_blank())
+
+# net footprints per country
+b <- results_all[, exp := fifelse(country_origin == country_target, "DOM", "EXP")][, .(d_f_a = sum(d_f_a, na.rm = T)), by = c("country_origin","exp")] %>% dcast(country_origin~exp, value.var = "d_f_a")
+c <- results_all[, imp := fifelse(country_origin == country_target, "DOM", "IMP")][, .(d_f_a = sum(d_f_a, na.rm = T)), by = c("country_target","imp")] %>% dcast(country_target~imp, value.var = "d_f_a")
+c1 <- c[country_target %in% setdiff(c$country_target,b$country_origin)] %>% rename(country = country_target)
+
+a <- left_join(b, c[,c(1,3)], by = c("country_origin"="country_target")) %>% rename(country = country_origin)
+a <- rbind(a,c1, fill = T)
+a[is.na(a)] <- 0
+a[, `:=` (NFP = DOM + IMP,
+          type = fcase(IMP > DOM, "NI",
+                       EXP > DOM, "NE",
+                       DOM > IMP, "DC",
+                       default = NA_character_))]
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+world <- world %>% select(admin, adm0_a3,geometry)
+world <- world %>% left_join(a[,c(1,5,6)], by = c("adm0_a3"="country"))
+
+
+ggplot(data = world) +
+  geom_sf(aes(fill = type, alpha = NFP)) +
+  coord_sf(crs = "+proj=robin") +
+  scale_alpha(range = c(0.4, 1)) +  # Optional tweak for visibility
+  theme_minimal()
+
+
+# Scale NFP within each type
+world <- world %>%
+  group_by(type) %>%
+  mutate(NFP_scaled = rescale(NFP)) %>%
+  ungroup()
+
+# Plot with fill by type and alpha by NFP within type
+ggplot(world) +
+  geom_sf(aes(fill = type, alpha = NFP_scaled)) +
+  scale_alpha(range = c(0.4, 1), guide = "none") +  # hide alpha legend
+  coord_sf(crs = "+proj=robin") +
+  theme_minimal() +
+  scale_fill_viridis_d(option = "H", begin = 0.1)
+  
+  scale_fill_brewer(palette = "Set2", name = "Type", direction = -1)
+
+ggsave(plot = last_plot(), bg = "#ffffff",
+         filename = "./output/FIG1-net-footprints-trade.png",
+         width = 240, height = 140, units = "mm", scale = 1)
 
 
 # top crops
 a <- results_all %>% group_by(crop) %>% summarise(d_f_a = sum(d_f_a), .groups = "drop") %>% arrange(desc(d_f_a)) %>% print(n = 15)
 
-a_l <- a$product[1:9]
+a_l <- a$crop[1:9]
 
-results_all %>% group_by(crop, consumption_category) %>% summarise(d_f_a = sum(d_f_a), .groups = "drop") %>% arrange(desc(d_f_a)) %>%
-  mutate(crop = if_else(crop %in% a_l, crop, "Other")) %>% group_by(crop, consumption_category) %>%
+results_all %>% mutate(trade = if_else(continent_origin == continent_target, "DOM", "TRADE")) %>%
+  group_by(crop, consumption_category,trade) %>% summarise(d_f_a = sum(d_f_a), .groups = "drop") %>% arrange(desc(d_f_a)) %>%
+  mutate(crop = if_else(crop %in% a_l, crop, "Other")) %>% group_by(crop, consumption_category,trade) %>%
   summarise(d_f_a = sum(d_f_a), .groups = "drop") %>% print(n = 15) %>%
   mutate(crop = fct_reorder(crop, d_f_a, .fun = sum, .desc = FALSE)) %>%  # Orders by sum(d_f_a)
   mutate(crop = fct_relevel(crop, "Other", after = 0)) %>%  # Moves "Other" to the bottom
-  ggplot(aes(x = crop, y = d_f_a, fill = consumption_category)) +
+  ggplot(aes(x = crop, y = d_f_a, fill = interaction(consumption_category,trade))) +
   geom_bar(stat = "identity", position = position_stack(reverse = TRUE)) +
   scale_fill_viridis_d(option = "viridis", direction = -1, begin = 0, end = 1) +  # Apply viridis color scale
   coord_flip() +
